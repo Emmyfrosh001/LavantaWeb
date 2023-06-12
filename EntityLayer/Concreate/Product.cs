612871a5af40d215691fe43189bafa014556cebd
Halil ÖZTÜRK <34791177+Halil4807@users.noreply.github.com>
2023-06-13 01:32:25 +0300

    Product tablosunun oluştuturlması

---
+++ EntityLayer/Concreate/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concreate
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public int ProductCategoryID { get; set; }
+        public int ProductStock { get; set; }
+        public float ProductPrice { get; set; }
+        public string ProductNote { get; set; }
+        public float ProductDiscount { get; set; }
+        public bool ProductDiscountStatus { get; set; }
+        public bool ProductStatus { get; set; }
+        public int ProductDetailObserveCount { get; set; }
+        public int ProductBasketInsertCount { get; set; }
+        public int ProductBasketDeleteCount { get; set; }
+        public int ProductSellCount { get; set;}
+        public int ProductReturnCount { get; set;}
+    }
+}
