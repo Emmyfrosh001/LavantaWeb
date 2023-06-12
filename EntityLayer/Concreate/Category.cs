331c05372a2dacb7af9d53e298b8f2e5a08c3144
Halil ÖZTÜRK <34791177+Halil4807@users.noreply.github.com>
2023-06-13 00:54:45 +0300

    Cateory Classının oluşturulması

---
+++ EntityLayer/Concreate/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concreate
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public int CategoryUst { get; set; }
+        public bool CategoryState { get; set; }
+    }
+}
