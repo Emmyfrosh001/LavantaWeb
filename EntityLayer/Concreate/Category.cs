77c8f2e39784800a35bbe1b2d3cb3fdd687560e7
Halil ÖZTÜRK <34791177+Halil4807@users.noreply.github.com>
2023-06-24 12:29:03 +0300

    Kategori Yorum

--- EntityLayer/Concreate/Category.cs
+++ EntityLayer/Concreate/Category.cs
@@ -8,10 +8,10 @@ namespace EntityLayer.Concreate
 {
     public class Category
     {
-        public int CategoryID { get; set; }
-        public string CategoryName { get; set; }
-        public int CategoryUst { get; set; }
-        public bool CategoryStatus { get; set; }
+        public int CategoryID { get; set; }     //Kategori ID
+        public string CategoryName { get; set; }     //Kategori Adı
+        public int CategoryUst { get; set; }     //Kategorinin Üst Kategorisi        {0 En üst Kategori }
+        public bool CategoryStatus { get; set; }         //Kategorinin Dueumu
 
         public ICollection<Product> Products { get; set; }  //Ürün tablosu ile ilişkilendirme
     }
