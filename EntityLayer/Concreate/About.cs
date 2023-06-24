c44ac7055182fa1a5b375041b5b5c3da8ab98470
Halil ÖZTÜRK <34791177+Halil4807@users.noreply.github.com>
2023-06-24 12:26:38 +0300

    Hakkımızda Tablosnuna Yorum satırı ekleme

--- EntityLayer/Concreate/About.cs
+++ EntityLayer/Concreate/About.cs
@@ -8,10 +8,10 @@ namespace EntityLayer.Concreate
 {
     public class About
     {
-        public int AboutID { get; set; }
-        public string AboutDetails { get; set;}
-        public string AboutDetails2 { get; set;}
-        public string AboutImage { get; set;}
-        public string AboutImage2 { get; set;}
+        public int AboutID { get; set; }        //Hakkımızda ID
+        public string AboutDetails { get; set;}     //Hakkımızda Detay 1
+        public string AboutDetails2 { get; set; }     //Hakkımızda Detay 2
+        public string AboutImage { get; set; }     //Hakkımızda Resim 1
+        public string AboutImage2 { get; set; }           //Hakkımızda Resim 2
     }
 }
