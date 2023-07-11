6a84814ff4413ff8e9251d57ce463f121840f40d
Halil ÖZTÜRK <34791177+Halil4807@users.noreply.github.com>
2023-07-11 07:44:07 +0300

    prodoct image ve contact belirleyici girilmesi

--- EntityLayer/Concreate/Contact.cs
+++ EntityLayer/Concreate/Contact.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,15 @@ namespace EntityLayer.Concreate
 {
     public class Contact
     {
+        [Key]
         public int ContactID { get; set; }      //İletişim Bilgi ID
+        [StringLength(50)]
         public string UserName { get; set; }    //Kullanıcı Adı
+        [StringLength(50)]
         public string UserMail { get; set; }    //Kullanıcı Mail Adresi
+        [StringLength(50)]
         public string Subject { get; set; }     //Mesajın Konusu
+        [StringLength(250)]
         public string Message { get; set; }     //Mesaj
     }
 }
--- EntityLayer/Concreate/ProductImage.cs
+++ EntityLayer/Concreate/ProductImage.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,11 @@ namespace EntityLayer.Concreate
 {
     public  class ProductImage
     {
+        [Key]
         public int ProductImageID { get; set; }    //Ürün Resim ID
+        [StringLength(150)]
         public string ProductImageAdress { get; set; }    //Ürün Resim Yolu
-        public string ProductImageStatus { get; set; }    //Ürün Resim Durumu
+        public bool ProductImageStatus { get; set; }    //Ürün Resim Durumu
 
         public int ProductID { get; set; }  //Ürün ID
         public virtual Product Product { get; set; }    //Ürün Tablosu ile ilişkilendirme
