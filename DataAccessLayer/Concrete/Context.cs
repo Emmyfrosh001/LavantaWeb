403404c2f56ae3b5ba7cedad66a26ed196524571
Halil ÖZTÜRK <34791177+Halil4807@users.noreply.github.com>
2023-07-11 23:37:55 +0300

    Entity Layer klasör concrete olarak değiştirilmesi ve Context sınıfının oluşturulması

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,23 @@
+﻿using EntityLayer.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context:DbContext
+    {
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments{ get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
