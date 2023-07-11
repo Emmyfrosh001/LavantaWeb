175fd84c6e80ee747a66c44c131f2e68fd0b5343
Halil ÖZTÜRK <34791177+Halil4807@users.noreply.github.com>
2023-07-11 07:35:58 +0300

    Order DataAnnodatations Tanımı

--- EntityLayer/Concreate/Order.cs
+++ EntityLayer/Concreate/Order.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +9,21 @@ namespace EntityLayer.Concreate
 {
     public class Order  //Sipariş
     {
+        [Key]
         public int OrderID { get; set; } //Sipariş ID
         public DateTime OrderDateTime { get; set; } //Sipariş Tarihi
+
+        [StringLength(50)]
         public string OrderState { get; set; } //Sipariş Güncel Durumu
+
+        [StringLength(50)]
         public string OrderPayType { get; set; } //Sipariş Ödeme Türü
         public float OrderPrice { get; set; }    //Sipariş Tutarı
+
+        [StringLength(250)]
         public string OrderAddress { get; set; } //Sipariş Adresi
+
+        [StringLength(50)]
         public string OrderCargoPrice { get; set; } //Sipariş Kargo Ücreti
 
         public int UserID { get; set; }  //Kullanıcı ID
