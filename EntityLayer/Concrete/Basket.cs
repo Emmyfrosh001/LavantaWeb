7b5e9e69fee5770caf49f4ce7679667d0ecaa8b4
Halil ÖZTÜRK <34791177+Halil4807@users.noreply.github.com>
2023-08-02 07:20:13 +0300

    sepet tablosuna etribüt eklenmesi

--- EntityLayer/Concrete/Basket.cs
+++ EntityLayer/Concrete/Basket.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace EntityLayer.Concrete
 {
     public  class Basket
     {
+        [Key]
         public int BasketID { get; set; }
 
         public int UserID { get; set; }//Kullanıcı ID
