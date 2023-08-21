37772482ea5eb9422f32dd2f7a8bdfbc76216d65
Halil ÖZTÜRK <34791177+Halil4807@users.noreply.github.com>
2023-08-21 14:49:02 +0300

    Category kontroller eklenmesi ve kodun ilk derlenmesi

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,32 @@
+﻿using DataAccessLayer.Concrete.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager
+    {
+        GenericRepository<Category> repository =new GenericRepository<Category>();
+
+        public List<Category> GetAllBl()
+        {
+            return repository.List();
+        }
+        public void CategoryAddBl(Category parametre)
+        {
+            if(parametre.CategoryName==""||parametre.CategoryName.Length<3||parametre.CategoryName.Length>=51)
+            {
+                //hata mesajı
+            }
+            else
+            {
+                repository.Add(parametre);
+            }
+        }
+    }
+}
