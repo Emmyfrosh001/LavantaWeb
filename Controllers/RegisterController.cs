cf296e9fbd9d419f6fed789fa60e7101b3ca6fb7
Halil ÖZTÜRK <34791177+Halil4807@users.noreply.github.com>
2024-02-14 02:49:03 +0300

    register formu  düzenleme

--- Controllers/RegisterController.cs
+++ Controllers/RegisterController.cs
@@ -9,6 +9,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
@@ -29,46 +30,59 @@ namespace AydinogluLavender.Controllers
         [HttpPost]
         public ActionResult Index(User kullanici)
         {
-            if (um.GetUserMail(kullanici.UserMail) == null)
+            if(TestEmailRegex(kullanici.UserMail))
             {
-                kullanici.UserStatus = true;
-                kullanici.UserPassword = MD5Hash(kullanici.UserPassword);
-                ValidationResult results = userValidator.Validate(kullanici);
-                if (results.IsValid)
+
+                if (um.GetUserMail(kullanici.UserMail) == null)
                 {
-                    um.AddUserBl(kullanici);
-                    Context c = new Context();
-                    string Encrypted = MD5Hash(DateTime.Now.ToString());
+                    kullanici.UserStatus = true;
+                    kullanici.UserPassword = MD5Hash(kullanici.UserPassword);
+                    ValidationResult results = userValidator.Validate(kullanici);
+                    if (results.IsValid)
+                    {
+                        um.AddUserBl(kullanici);
+                        Context c = new Context();
+                        string Encrypted = MD5Hash(DateTime.Now.ToString());
 
-                    HttpCookie AydinogluLavenderCookie = new HttpCookie("AydinogluLavender");
-                    AydinogluLavenderCookie["UserMail"] = kullanici.UserMail;
-                    AydinogluLavenderCookie.Expires = DateTime.Now.AddDays(10);
-                    Response.Cookies.Add(AydinogluLavenderCookie);
+                        HttpCookie AydinogluLavenderCookie = new HttpCookie("AydinogluLavender");
+                        AydinogluLavenderCookie["UserMail"] = kullanici.UserMail;
+                        AydinogluLavenderCookie.Expires = DateTime.Now.AddDays(10);
+                        Response.Cookies.Add(AydinogluLavenderCookie);
 
-                    HttpCookie LoginCookie = new HttpCookie("LoginData");
-                    LoginCookie.Values.Add("Data", Encrypted);
-                    LoginCookie.Expires = DateTime.Now.AddDays(10);
-                    Response.Cookies.Add(LoginCookie);
+                        HttpCookie LoginCookie = new HttpCookie("LoginData");
+                        LoginCookie.Values.Add("Data", Encrypted);
+                        LoginCookie.Expires = DateTime.Now.AddDays(10);
+                        Response.Cookies.Add(LoginCookie);
 
-                    FormsAuthentication.SetAuthCookie(kullanici.UserMail, true);
-                    Session["UserMail"] = kullanici.UserMail;
+                        FormsAuthentication.SetAuthCookie(kullanici.UserMail, true);
+                        Session["UserMail"] = kullanici.UserMail;
 
-                    kullanici.LoginInfo = Encrypted;
-                    um.UpdateUserBl(kullanici);
+                        kullanici.LoginInfo = Encrypted;
+                        um.UpdateUserBl(kullanici);
 
-                    return RedirectToAction("Index", "Home");
+                        return RedirectToAction("Index", "Home");
+
+                    }
+                    else
+                    {
+                        foreach (var item in results.Errors)
+                        {
+                            ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+
+                        }
+                    }
 
                 }
                 else
                 {
-                    foreach (var item in results.Errors)
-                    {
-                        ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
 
-                    }
+                    Session["MailInfo"] = "Kayitli";
                 }
             }
-            Session["MailInfo"] = "Kayitli";
+            else
+            {
+                Session["MailInfo"] = "Gecersiz";
+            }
             return View();
         }
         public static string MD5Hash(string text)
@@ -82,6 +96,18 @@ namespace AydinogluLavender.Controllers
                 sb.Append(ba.ToString("x2").ToLower());
             }
             return sb.ToString();
+        }
+        public bool TestEmailRegex(string emailAddress)
+        {
+            string patternStrict = @"^(([^<>()[\]\\.,;:\s@\""]+"
+            + @"(\.[^<>()[\]\\.,;:\s@\""]+)*)|(\"".+\""))@"
+            + @"((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}"
+            + @"\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+"
+            + @"[a-zA-Z]{2,}))$";
+            Regex reStrict = new Regex(patternStrict);
+            bool isStrictMatch = reStrict.IsMatch(emailAddress);
+            return isStrictMatch;
+
         }
     }
 }
